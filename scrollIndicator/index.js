f140b3ba7565a4d0d7228243073d7015680fdd56
Ankita Doshi <40891409+Ankitadoshi@users.noreply.github.com>
2019-05-22 19:36:09 +0530

    scroll indicator after the header

---
+++ scrollIndicator/index.js
@@ -0,0 +1,8 @@
+
+function scroll(){
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+window.onscroll = function() {scroll()};
