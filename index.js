d4a6cb9abd15dedc74b8d8d07ae783d08155e45c
Ankita Doshi <40891409+Ankitadoshi@users.noreply.github.com>
2019-05-22 19:37:06 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-function scroll(){
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
-}
-window.onscroll = function() {scroll()};
